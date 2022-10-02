f90444ea1aeed12dea4e3d7506e476991f5ac7da
Gordon Mo <gmo30@stuy.edu>
2022-10-02 15:38:47 -0400

    added dict for numcrunch

--- 06_py-csv/numbercruncher.py
+++ 06_py-csv/numbercruncher.py
@@ -3,11 +3,21 @@ occupations_file = open("occupations.csv", "r")
 content = occupations_file.read()
 #print(content)
 
-library = {Job Class:[],
-           Percentage:[]
+library = {"Job Class":[],
+           "Percentage":[]
     }
 
-new_content = content.split("\n")
+new_content = content.split('\n')
+#print(new_content)
+
+temp = []
 for i in range(len(new_content)-1):
-    new_content = new_content.split(",")
-print(new_content)
+    temp += (new_content[i].rsplit(",",1))
+#print(temp)
+
+for i in range(2,len(temp)-2,2):
+    #print(i)
+    library["Job Class"].append(temp[i])
+    library["Percentage"].append(float(temp[i+1]))
+    
+print(library)
