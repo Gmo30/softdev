f7beb2acd3c1f553c7b3a36c5e4c2a63f86a03d3
Gordon Mo <gmo30@stuy.edu>
2022-10-02 16:11:43 -0400

    added using csv

--- 05_bitstream/krewes.py
+++ 05_bitstream/krewes.py
@@ -11,37 +11,54 @@ QCC: open() is used to open txt files with the second parameter "r" to read the 
 """
 import random
 
-krewes_file = open("krewes.txt", "r")
+#krewes_file = open("krewes.txt", "r")
+krewes_file = open("devofam.csv", "r")
 content = krewes_file.read()
-#print(content.split("$$$",1))
+#print(content)
 library = {2:[],
            7:[],
            8:[]}
 
-new_content = content.split("@@@")
-#print(len(new_content))
+# #SPLIT FOR krewes.txt file
+# new_content = content.split("@@@")
+# #print(len(new_content))
+# 
+# #def return_devo():
+# for i in range(len(new_content)-1):
+#         #print(new_content[i])
+#     temp = new_content[i].split("$$$")
+#         #print(temp)
+#     if(temp[0] == "2"):   
+#         library[2].append(temp[1])
+#         library[2].append(temp[2])
+#     elif(temp[0] == "7"):
+#         library[7].append(temp[1])
+#         library[7].append(temp[2])
+#     elif(temp[0] == "8"):
+#         library[8].append(temp[1])
+#         library[8].append(temp[2])
+# #return library
 
-#def return_devo():
+#SPLIT FOR devofam.csv file
+new_content = content.split("\n")
+#print(new_content)
 for i in range(len(new_content)-1):
-        #print(new_content[i])
-    temp = new_content[i].split("$$$")
-        #print(temp)
-    if(temp[0] == "2"):   
-        library[2].append(temp[1])
+    temp = new_content[i].split(",")
+    if(temp[1] == "2"):   
+        library[2].append(temp[0])
         library[2].append(temp[2])
-    elif(temp[0] == "7"):
-        library[7].append(temp[1])
+    elif(temp[1] == "7"):
+        library[7].append(temp[0])
         library[7].append(temp[2])
-    elif(temp[0] == "8"):
-        library[8].append(temp[1])
+    elif(temp[1] == "8"):
+        library[8].append(temp[0])
         library[8].append(temp[2])
-#return library
 
-periods = [2,7, 8]
+periods = [2,7,8]
 period = random.choice(periods)
 temp = library[period]
 ducky = random.randrange(1,len(temp),2)
-devo = ducky -1
+devo = ducky-1
 
 
 print("Period:", period, "Devo:" ,temp[devo],"Ducky:",  temp[ducky])
