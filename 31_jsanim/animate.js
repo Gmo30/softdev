8c8c35d81faf6c567c6cffabd8cb0ca25e76c083
Gordon Mo <gmo30@stuy.edu>
2023-04-25 21:34:05 -0400

    31

--- 31_jsanim/animate.js
+++ 31_jsanim/animate.js
@@ -1,3 +1,11 @@
+/*
+Team Ringbearers :: Gordon Mo, Hui Wang 
+       SoftDev pd7
+       K31 -- canvas based JS animation
+       2023-04-25t
+       HTML file for JavaScript canvas work
+*/
+
 var c = document.getElementById("playground");
 
 var dotButton = document.getElementById("buttonCircle");
@@ -17,19 +25,32 @@ var radius = 0;
 var growing = true ;
 
 var drawDot = () => {
-    clear
+    clear()
     ctx.beginPath()
     ctx.arc(c.width/2, c.height/2, radius, 0, 2 * Math.PI);
     ctx.stroke()
+    ctx.fill()
     if(growing){
+        window.cancelAnimationFrame(requestID)
         radius += 1;  
     }
+    else{
+        window.cancelAnimationFrame(requestID)
+        radius -= 1;
+    }
 
-    globalID = window.requestAnimationFrame(drawDot);
+    if(radius == c.width/2){
+        growing = false;
+    }
+    else if(radius <= 0){
+        growing = true;
+    }
+
+    requestID = window.requestAnimationFrame(drawDot);
 }
 
 var stopIt = () =>{
-    window.cancelAnimationFrame(globalID)
+    window.cancelAnimationFrame(requestID)
 }
 
 dotButton.addEventListener( "click", drawDot);
